e74bf64e3df1016a1a6a90ecd0b400b6ed2fb331
hyperpolymath <6759885+hyperpolymath@users.noreply.github.com>
2026-06-07 23:32:08 +0100

    Apply estate standardization: governance docs, contractiles, CI/CD cleanup

--- contractiles/runners/must/src/manifest.rs
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MPL-2.0
-// Copyright (c) Jonathan D.A. Jewell <j.d.a.jewell@open.ac.uk>
-// src/manifest.rs
-#[cfg(feature = "nickel")]
-pub fn parse_nickel_manifest(path: &str) -> Result<MustFile, String> {
-    use nickel_lang::eval::eval_file;
-    let value = eval_file(path).map_err(|e| format!("Nickel error: {}", e))?;
-    serde_json::from_value(value.into()).map_err(|e| e.to_string())
-}
